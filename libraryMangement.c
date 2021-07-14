85b30a8346f4ce5f10dd231535f52ceb634f3ae7
Deepak M S <souparnikad40@gmail.com>
2021-07-14 20:22:13 +0530

    project details and syntx added

---
+++ libraryMangement.c
@@ -0,0 +1,27 @@
+/***********************************************************************************
+ * ######################### PROJECT DETAILS #################################### *
+ * AUTHOR : DEEPAK M S
+ * AIM    : Create a structure containing book information like accession number, 
+            name of book and name of author. The maximum number of books in the 
+            library is1000. Also create and display a menu as follows: 
+            (i) Add a new book. 
+            (ii) Display book information in lexicographical order of book names. 
+            (iii) Display the number of books of a particular author  .
+            (Use separate function for sorting .)  
+
+***********************************************************************************/
+
+
+//header files
+#include <stdio.h>
+#include <string.h>
+
+//function declartions
+
+//global variables
+int i;
+
+//main function
+int main(){
+    return 0;
+}
